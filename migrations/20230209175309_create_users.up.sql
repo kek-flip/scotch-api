b41bb63bc66894095ed32cc54136c9a335faa492
Maxim Davydov <norm.pochta.maxa@gmail.com>
2023-02-09 21:55:29 +0300

    Добавил миграции пользователей

---
+++ migrations/20230209175309_create_users.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    login VARCHAR(25) CONSTRAINT min_login_length CHECK(LENGTH(login) >= 3) NOT NULL,
+    encrypted_password VARCHAR(100) CONSTRAINT min_password_length CHECK(LENGTH(encrypted_password) >= 3) NOT NULL,
+    name VARCHAR(25) NOT NULL,
+    age INTEGER CHECK(age BETWEEN 18 AND 99) NOT NULL,
+    gender VARCHAR(6) NOT NULL CHECK(gender = 'male' OR gender = 'female'),
+    city VARCHAR(25) NOT NULL,
+    phone_number VARCHAR(12) NOT NULL UNIQUE,
+    about TEXT
+);
