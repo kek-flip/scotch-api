a3bed9b949e1451eac2f67c2317c325f14a896be
Maxim Davydov <norm.pochta.maxa@gmail.com>
2023-03-30 20:26:25 +0300

    Рефакторинг репозитория совпадений: Изменил функцию поиска по пользователю

--- internal/store/matchrepository.go
+++ internal/store/matchrepository.go
@@ -2,7 +2,6 @@ package store
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/kek-flip/scotch-api/internal/model"
 )
@@ -26,22 +25,21 @@ func (r *MatchRepository) Create(m *model.Match) error {
 	return row.Scan(&m.ID)
 }
 
-func (r *MatchRepository) find(field string, value interface{}) ([]*model.Match, error) {
+func (r *MatchRepository) FindByUser(userID int) ([]*model.Match, error) {
 	matches := make([]*model.Match, 0)
 
 	rows, err := r.s.db.Query(
 		context.Background(),
-		fmt.Sprintf("SELECT * FROM matches WHERE %s = $1", field),
-		value,
+		"SELECT * FROM matches WHERE user_1 = $1 OR user_2 = $1",
+		userID,
 	)
 
 	if err != nil {
 		return nil, err
 	}
 
+	m := &model.Match{}
 	for rows.Next() {
-		m := &model.Match{}
-
 		err := rows.Scan(
 			&m.ID,
 			&m.User1,
@@ -52,30 +50,14 @@ func (r *MatchRepository) find(field string, value interface{}) ([]*model.Match,
 			return nil, err
 		}
 
+		if m.User1 != userID {
+			m.User1, m.User2 = m.User2, m.User1
+		}
+
 		matches = append(matches, m)
 	}
 
 	return matches, nil
-}
-
-func (r *MatchRepository) FindById(id int) (*model.Match, error) {
-	matches, err := r.find("match_id", id)
-	return matches[0], err
-}
-
-func (r *MatchRepository) FindByUser(userId int) ([]*model.Match, error) {
-	matchesBy1, err1 := r.find("user_1", userId)
-	matchesBy2, err2 := r.find("user_2", userId)
-
-	if err1 != nil && err2 != nil {
-		return nil, err1
-	}
-
-	for i := range matchesBy2 {
-		matchesBy2[i].User1, matchesBy2[i].User2 = matchesBy2[i].User2, matchesBy2[i].User1
-	}
-
-	return append(matchesBy1, matchesBy2...), nil
 }
 
 func (r *MatchRepository) DeleteByUser(id int) error {
