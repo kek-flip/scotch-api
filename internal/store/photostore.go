6a90e5ff53822a28b6de9b2ab190ef1b88763c09
Maxim Davydov <norm.pochta.maxa@gmail.com>
2023-04-01 11:13:07 +0300

    Добавил репозиторий фотографий

---
+++ internal/store/photostore.go
@@ -0,0 +1,64 @@
+package store
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+type PhotoStore struct {
+	path string
+}
+
+var errDirNotExist = errors.New("directory does not exist")
+
+func NewPhotoStore(path string) (*PhotoStore, error) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return nil, errDirNotExist
+	}
+
+	return &PhotoStore{path}, nil
+}
+
+func (ps *PhotoStore) Create(photo []byte, fileName string) error {
+	fullFileName := fmt.Sprintf("%s/%s.jpeg", ps.path, fileName)
+	pf, err := os.Create(fullFileName)
+	if err != nil {
+		return err
+	}
+	defer pf.Close()
+
+	n, err := pf.Write(photo)
+	if len(photo) != n {
+		os.Remove(fullFileName)
+		return err
+	}
+
+	return nil
+}
+
+func (ps *PhotoStore) FindByName(fileName string) ([]byte, error) {
+	fullFileName := fmt.Sprintf("%s/%s.jpeg", ps.path, fileName)
+	pf, err := os.OpenFile(fullFileName, os.O_RDONLY, 0666)
+	if err != nil {
+		return nil, err
+	}
+	defer pf.Close()
+
+	var p bytes.Buffer
+	pw := bufio.NewWriter(&p)
+	_, err = io.Copy(pw, pf)
+	if err != nil {
+		return nil, err
+	}
+
+	return p.Bytes(), nil
+}
+
+func (ps *PhotoStore) DeleteByName(fileName string) error {
+	fullFileName := fmt.Sprintf("%s/%s.jpeg", ps.path, fileName)
+	return os.Remove(fullFileName)
+}
